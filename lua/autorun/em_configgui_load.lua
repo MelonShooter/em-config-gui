026d12a25bd1df3dab21d2de88ed3da077816413
MelonShooter <14815068+MelonShooter@users.noreply.github.com>
2020-01-01 21:53:13 -0800

    Update em_configgui_load.lua

--- lua/autorun/em_configgui_load.lua
+++ lua/autorun/em_configgui_load.lua
@@ -1,8 +1,14 @@
 if SERVER then
 	AddCSLuaFile("em_configgui/client/cl_configgui_base.lua")
+	AddCSLuaFile("em_configgui/client/cl_configgui_configoption.lua")
+	AddCSLuaFile("em_configgui/client/cl_configgui_configpanel.lua")
+	AddCSLuaFile("em_configgui/client/cl_configgui_numslider.lua")
 	include("em_configgui/server/sv_configgui_base.lua")
 end
 
 if CLIENT then
 	include("em_configgui/client/cl_configgui_base.lua")
+	include("em_configgui/client/cl_configgui_configoption.lua")
+	include("em_configgui/client/cl_configgui_configpanel.lua")
+	include("em_configgui/client/cl_configgui_numslider.lua")
 end
